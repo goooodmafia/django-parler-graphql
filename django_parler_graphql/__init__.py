38ed8b9d59c7ff522c8be2b3bbe1ed468d6c48ba
selmanceker <selmanceker@gmail.com>
2020-04-25 16:48:09 +0300

    apps added

---
+++ django_parler_graphql/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'django_parler_graphql.apps.DjangoParlerGraphqlConfig'
---
+++ django_parler_graphql/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoParlerGraphqlConfig(AppConfig):
+    name = 'django_parler_graphql'
+    verbose_name = "Django Parler Graphql"
